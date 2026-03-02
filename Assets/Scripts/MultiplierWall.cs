618d8cbfae3ae32b262c5a3c823093c622b8534e
x3rdev <nicolaeungur@gmail.com>
2026-03-01 16:01:06 -0800

    textmesh

--- Assets/Scripts/MultiplierWall.cs
+++ Assets/Scripts/MultiplierWall.cs
@@ -1,7 +1,26 @@
 using UnityEngine;
+using TMPro;
 
 public class MultiplierWall : MonoBehaviour
 {
+    public enum ModifierType
+    {
+        Add,
+        Sub,
+        Mul,
+        Div
+    }
+
+    [Header("Gate Settings")]
+    public ModifierType operation;
+    public int value;
+
+    [Header("Visuals")]
+    [SerializeField] private TextMeshPro gateText;
+    [SerializeField] private MeshRenderer gateRenderer;
+    [SerializeField] private Color positiveColor = Color.blue;
+    [SerializeField] private Color negativeColor = Color.red;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
